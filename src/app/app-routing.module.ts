28202ba73b8100e5a02d81c3824d945a848436c2
MicrowavedBunny <jayz34567@gmail.com>
2021-12-17 17:01:32 -0500

    route set up for register page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { CourseDetailComponent } from './components/course-components/course-det
 import { AddCourseComponent } from './components/course-components/add-course/add-course.component';
 import { RemoveCourseComponent } from './components/course-components/remove-course/remove-course.component';
 import { LoginPageComponent } from './components/student-components/login-page/login-page.component';
+import { RegisterPageComponent } from './components/student-components/register-page/register-page.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login-page' },
@@ -13,7 +14,8 @@ const routes: Routes = [
   { path: 'edit-course/:id', component: CourseDetailComponent},
   { path: 'add-course', component: AddCourseComponent},
   { path: 'remove-course/:id', component: RemoveCourseComponent},
-  { path: 'login-page', component: LoginPageComponent}
+  { path: 'login-page', component: LoginPageComponent},
+  { path: 'register-page', component: RegisterPageComponent}
 ];
 
 @NgModule({
