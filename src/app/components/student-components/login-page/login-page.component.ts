54be2a59753b5f4dfaaa8339bda21133310fd18c
MicrowavedBunny <jayz34567@gmail.com>
2021-12-17 23:12:45 -0500

    created the login page with no functionallity

--- src/app/components/student-components/login-page/login-page.component.ts
+++ src/app/components/student-components/login-page/login-page.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
+import { CrudService } from 'src/app/service/crud.service';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  register: FormGroup;
 
-  ngOnInit(): void {
+  constructor(    
+    public formBuilder: FormBuilder,
+    private router: Router,
+    private ngZone: NgZone,
+    private crudService: CrudService
+  ) {
+    this.register = this.formBuilder.group({})
+    
+    this.loginForm = this.formBuilder.group({
+      Email: [''],
+      Password: ['']
+    })
   }
+  ngOnInit(): void { }
 
+  onLogin(): any { 
+this.ngZone.run(() => this.router.navigateByUrl('/course-list'))
+}  
+onRegisterClick(): any { 
+this.ngZone.run(() => this.router.navigateByUrl('/register-page'))
+}
 }
