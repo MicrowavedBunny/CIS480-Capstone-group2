40bccbc7e420df0a479c19eac66b9c46d8edb8ac
MicrowavedBunny <jayz34567@gmail.com>
2022-01-04 13:57:22 -0500

    added ts for form number 3

--- src/app/components/course-components/add-course/add-course.component.ts
+++ src/app/components/course-components/add-course/add-course.component.ts
@@ -13,6 +13,7 @@ export class AddCourseComponent implements OnInit {
 
   courseForm: FormGroup;
   courseForm2: FormGroup;
+  courseForm3: FormGroup;
 
   constructor(
     public formBuilder: FormBuilder,
@@ -38,6 +39,15 @@ export class AddCourseComponent implements OnInit {
       cap: [''],
       owner: [localStorage.getItem("studentId")]  //this cant be '' or it will clear the form value (if it is set in the hidden form inside html) this is where we need to pass in the user id from the other collection
     })
+    this.courseForm3 = this.formBuilder.group({
+      name: [''],
+      code: [''],
+      description: [''],
+      credits: [''],
+      type: [''],
+      cap: [''],
+      owner: [localStorage.getItem("studentId")]  //this cant be '' or it will clear the form value (if it is set in the hidden form inside html) this is where we need to pass in the user id from the other collection
+    })
    }
 
   ngOnInit(): void {  }
@@ -52,6 +62,13 @@ export class AddCourseComponent implements OnInit {
         console.log(err);
       });
       this.crudService.AddCourse(this.courseForm2.value)
+      .subscribe(() => {
+        console.log('Data added successfully')
+        this.ngZone.run(() => this.router.navigateByUrl('/course-list'))
+      }, (err) => {
+        console.log(err);
+      });
+      this.crudService.AddCourse(this.courseForm3.value)
       .subscribe(() => {
         console.log('Data added successfully')
         this.ngZone.run(() => this.router.navigateByUrl('/course-list'))
